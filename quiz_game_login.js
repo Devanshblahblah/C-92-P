810722c1863dc40695ed4bbc81971689858432d9
Devanshblahblah <114562971+Devanshblahblah@users.noreply.github.com>
2023-05-27 16:55:48 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,10 @@
+function adduser()
+{
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("Player1Name", player1_name);
+    localStorage.setItem("player2Name", player2_name);
+
+    window.location = "quiz_game_page.html";
+}
